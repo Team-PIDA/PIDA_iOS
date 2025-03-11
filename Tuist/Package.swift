1a6931293b6f50905ecbfeab99731f824b63998f
ir.__.si <yongin9805@gmail.com>
2025-03-11 17:39:14 +0900

    feat: #3 Package 추가

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -3,20 +3,12 @@ import PackageDescription
 
 #if TUIST
     import struct ProjectDescription.PackageSettings
-
-    let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,]
-        productTypes: [:]
-    )
+    let packageSettings = PackageSettings(productTypes: [:])
 #endif
 
 let package = Package(
     name: "PIDA_iOS",
     dependencies: [
-        // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
+        .package(url: "https://docs.tuist.io/documentation/tuist/dependencies", exact: "1.18.0")
     ]
 )
