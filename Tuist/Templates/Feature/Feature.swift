9ee4bd503f0701fcd600c6674a01c7d9f9bcffa1
ir.__.si <yongin9805@gmail.com>
2025-03-11 18:56:57 +0900

    feat: #3 Feature scaffold 세팅

---
+++ Tuist/Templates/Feature/Feature.swift
@@ -0,0 +1,64 @@
+//
+//  Feature.swift
+//  Templates
+//
+//  Created by 조용인 on 3/11/25.
+//
+
+import Foundation
+import ProjectDescription
+
+fileprivate let name: Template.Attribute = .required("name")
+fileprivate let author: Template.Attribute = .required("author")
+fileprivate let currentDate: Template.Attribute = .optional("currentDate", default: .string(DateFormatter().string(from: Date())))
+
+let template = Template(
+    description: "SwiftUI + TCA 기반 Feature 모듈 템플릿",
+    attributes: [
+        name,
+        author,
+        currentDate
+    ],
+    items: [
+        // Feature의 Project.swift 파일 생성
+        .file(
+            path: "Projects/Features/\(name)/Project.swift",
+            templatePath: "feature_project.stencil"
+        ),
+        // Feature의 Interface 폴더 및 파일 생성
+        .file(
+            path: "Projects/Features/\(name)/Interface/\(name)Interface.swift",
+            templatePath: "feature_interface.stencil"
+        ),
+        // Feature의 Implement 폴더 및 파일 생성
+        .file(
+            path: "Projects/Features/\(name)/Implement/\(name)Feature.swift",
+            templatePath: "feature_implement.stencil"
+        ),
+        // Feature의 Reducer 파일 (TCA)
+        .file(
+            path: "Projects/Features/\(name)/Implement/\(name)Reducer.swift",
+            templatePath: "feature_reducer.stencil"
+        ),
+        // Feature의 SwiftUI View 파일
+        .file(
+            path: "Projects/Features/\(name)/Implement/\(name)View.swift",
+            templatePath: "feature_view.stencil"
+        ),
+        // Feature의 Unit Test 폴더 및 파일 생성
+        .file(
+            path: "Projects/Features/\(name)/Testing/\(name)UnitTests.swift",
+            templatePath: "feature_tests.stencil"
+        ),
+        // Feature의 Demo (예제) 파일 생성
+        .file(
+            path: "Projects/Features/\(name)/Demo/Sources/AppDelegate.swift",
+            templatePath: "feature_demo_appdelegate.stencil"
+        ),
+        // Feature의 Resources 파일 생성
+        .file(
+            path: "Projects/Features/\(name)/Demo/Resources/Assets.xcassets",
+            templatePath: "feature_resources.stencil"
+        )
+    ]
+)
--- Tuist/Templates/empty.swift
+++
@@ -1,8 +0,0 @@
-//
-//  empty.swift
-//  Config
-//
-//  Created by 조용인 on 3/11/25.
-//
-
-import Foundation
