990f10f22f8e1b2829f8f205dcd053b47b3cea5f
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2025-03-14 15:40:54 +0900

    fix: #7 Feature, Interface 폴더 경로 수정

---
+++ Projects/Features/Map/MapDemo/Sources/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  MapDemo
+//
+//  Created by Jiyeon on 3/14/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
---
+++ Projects/Features/Map/MapDemo/Sources/MapViewRepresentable.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewRepresentable.swift
+//  MapDemo
+//
+//  Created by Jiyeon on 3/14/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import Foundation
