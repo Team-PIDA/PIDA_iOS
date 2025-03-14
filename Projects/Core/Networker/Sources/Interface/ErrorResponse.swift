81228dd6c19f9a08265fb0564b6d957537fba21a
ir.__.si <yongin9805@gmail.com>
2025-03-14 15:35:26 +0900

    feat: #4 Server에서 발생한 ErrorResponse

---
+++ Projects/Core/Networker/Sources/Interface/ErrorResponse.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorResponse.swift
+//  Networker
+//
+//  Created by 조용인 on 3/14/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import Foundation
+
+public struct ErrorResponse: Decodable, Sendable {
+  public let errorClassName: String
+  public let message: String
+}
