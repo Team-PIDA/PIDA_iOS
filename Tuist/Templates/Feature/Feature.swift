72d9bb66de62862427aa3bfd0f8c330f9cc6bb0e
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2025-03-15 16:22:31 +0900

    feat: #5 Feature Template 수정

--- Tuist/Templates/Feature/Feature.swift
+++ Tuist/Templates/Feature/Feature.swift
@@ -35,14 +35,19 @@ let featureTemplate = Template(
             path: "Projects/Features/\(name)/\(name)Feature/Sources/\(name)Feature.swift",
             templatePath: "feature_implement.stencil"
         ),
-        // Feature의 Reducer 파일 (TCA)
+        // Feature의 Reducer implement 파일 (TCA)
         .file(
             path: "Projects/Features/\(name)/\(name)Feature/Sources/\(name)Reducer.swift",
-            templatePath: "feature_reducer.stencil"
+            templatePath: "feature_reducer_implement.stencil"
+        ),
+        // Feature Reducer의 interface 파일
+        .file(
+          path: "Projects/Features/\(name)/\(name)FeatureInterface/Sources/\(name)Reducer.swift",
+          templatePath: "feature_reducer_interface.stencil"
         ),
         // Feature의 SwiftUI View 파일
         .file(
-            path: "Projects/Features/\(name)/\(name)Feature/Sources/\(name)View.swift",
+            path: "Projects/Features/\(name)/\(name)FeatureInterface/Sources/\(name)View.swift",
             templatePath: "feature_view.stencil"
         ),
         // Feature의 Unit Test 폴더 및 파일 생성
