e4eba33c4bd32bdf39eaf9cc622f38a743d2c165
ir.__.si <yongin9805@gmail.com>
2025-03-16 15:03:51 +0900

    feat: #6 사용자 정의 Size 상수 선언

---
+++ Projects/Core/DesignKit/Sources/Resource+/Size+.swift
@@ -0,0 +1,31 @@
+//
+//  Size+.swift
+//  DesignKit
+//
+//  Created by 조용인 on 3/16/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import Foundation
+
+public struct Size {
+  public static let Number0: CGFloat = 0
+  public static let Number2: CGFloat = 2
+  public static let Number4: CGFloat = 4
+  public static let Number8: CGFloat = 8
+  public static let Number10: CGFloat = 10
+  public static let Number12: CGFloat = 12
+  public static let Number16: CGFloat = 16
+  public static let Number18: CGFloat = 18
+  public static let Number20: CGFloat = 20
+  public static let Number24: CGFloat = 24
+  public static let Number28: CGFloat = 28
+  public static let Number32: CGFloat = 32
+  public static let Number40: CGFloat = 40
+  public static let Number48: CGFloat = 48
+  public static let Number56: CGFloat = 56
+  public static let Number64: CGFloat = 64
+  public static let Number72: CGFloat = 72
+  public static let Number80: CGFloat = 80
+  public static let Number100: CGFloat = 100
+}
