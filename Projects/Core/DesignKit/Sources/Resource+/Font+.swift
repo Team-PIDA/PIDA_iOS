78272b2be98b09b7c085acaf79d84783c036b106
ir.__.si <yongin9805@gmail.com>
2025-03-16 17:44:39 +0900

    fix: #6  Font 경로 수정

--- Projects/Core/DesignKit/Sources/Resource+/Font+.swift
+++ Projects/Core/DesignKit/Sources/Resource+/Font+.swift
@@ -8,40 +8,38 @@
 
 import Foundation
 import SwiftUI
-
 extension DesignKitFontFamily {
-  
-  public struct FontSet {
+  public struct FontSet: Sendable {
     public struct Heading: Sendable {
-      public static let heading1 = Font.custom("Pretendard-SemiBold", size: 24)
-      public static let heading2 = Font.custom("Pretendard-SemiBold", size: 20)
-      public static let heading3 = Font.custom("Pretendard-SemiBold", size: 18)
-      public static let heading4 = Font.custom("Pretendard-SemiBold", size: 16)
+      public static let heading1 =  DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 24)
+      public static let heading2 =  DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 20)
+      public static let heading3 =  DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 18)
+      public static let heading4 =  DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 16)
     }
     
     public struct Title: Sendable {
-      public static let title1 = Font.custom("Pretendard-SemiBold", size: 20)
-      public static let title2 = Font.custom("Pretendard-SemiBold", size: 18)
-      public static let title3 = Font.custom("Pretendard-SemiBold", size: 16)
-      public static let title4 = Font.custom("Pretendard-SemiBold", size: 14)
-      public static let title5 = Font.custom("Pretendard-SemiBold", size: 12)
+      public static let title1 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 20)
+      public static let title2 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 18)
+      public static let title3 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 16)
+      public static let title4 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 14)
+      public static let title5 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 12)
     }
     
     public struct Body: Sendable {
-      public static let body1 = Font.custom("Pretendard-SemiBold", size: 18)
-      public static let body2 = Font.custom("Pretendard-SemiBold", size: 16)
-      public static let body3 = Font.custom("Pretendard-SemiBold", size: 14)
+      public static let body1 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 18)
+      public static let body2 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 16)
+      public static let body3 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 14)
     }
     
     public struct Caption: Sendable {
-      public static let caption1 = Font.custom("Pretendard-SemiBold", size: 12)
-      public static let caption2 = Font.custom("Pretendard-SemiBold", size: 8)
+      public static let caption1 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 12)
+      public static let caption2 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 8)
     }
     
     public struct Label: Sendable {
-      public static let label1 = Font.custom("Pretendard-SemiBold", size: 16)
-      public static let label2 = Font.custom("Pretendard-SemiBold", size: 14)
-      public static let label3 = Font.custom("Pretendard-SemiBold", size: 12)
+      public static let label1 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 16)
+      public static let label2 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 14)
+      public static let label3 = DesignKitFontFamily.Pretendard.semiBold.swiftUIFont(size: 12)
     }
   }
 }
