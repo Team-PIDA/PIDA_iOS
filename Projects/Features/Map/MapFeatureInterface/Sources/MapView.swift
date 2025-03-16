6b63191db60843a3999eba2ac08753cd79a2a653
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2025-03-16 16:51:07 +0900

    feat: #13 현 위치 버튼 구현

--- Projects/Features/Map/MapFeatureInterface/Sources/MapView.swift
+++ Projects/Features/Map/MapFeatureInterface/Sources/MapView.swift
@@ -8,9 +8,14 @@
 
 import SwiftUI
 import ComposableArchitecture
+import DesignKit
 
 public struct MapView: View {
-  let store: StoreOf<MapReducer>
+  
+  private typealias Icons = DesignKitAsset.Icons
+  private typealias Colors = DesignKitAsset.ColorSet
+  
+  
   
   public init(store: StoreOf<MapReducer>) {
     self.store = store
@@ -19,6 +24,31 @@ public struct MapView: View {
   public var body: some View {
     MapViewRepresentable()
       .ignoresSafeArea()
+    VStack {
+      Spacer()
+      HStack {
+        Spacer()
+        curLocationButton()
+      }
+    }
+    .padding(.trailing, 16)
+      
+    }
+    
+  }
+  
+  
+  private func curLocationButton() -> some View {
+    Button {
+      store.send(.fetchUserLocation)
+    } label: {
+      Image(asset: Icons.myLocation).renderingMode(.template)
+    }
+    .foregroundStyle(Colors.Icon.Primary)
+    .frame(width: 48, height: 48)
+    .background(Colors.Background.Primary)
+    .clipShape(.circle)
+    .shadow(color: .black.opacity(0.2), radius: 4)
   }
 }
 
