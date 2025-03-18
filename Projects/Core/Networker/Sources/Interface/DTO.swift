9a4480cd9f38ade9661d79223d4422dd81014d24
ir.__.si <yongin9805@gmail.com>
2025-03-19 00:01:20 +0900

    DTO 및 UserDefault 관련 인터페이스 추가 (#17)
    * feat: #16 Network 모듈 DTO 인터페이스 추가
    
    * feat: #16 Network 모듈 DTO 인터페이스 추가
    
    * feat: #16 간단하게 사용 가능하도록 UserDefault 인터페이스화
    
    * chore: #16 fix indent

---
+++ Projects/Core/Networker/Sources/Interface/DTO.swift
@@ -0,0 +1,14 @@
+//
+//  DTO.swift
+//  Networker
+//
+//  Created by 조용인 on 3/18/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import Foundation
+
+protocol DTO: Decodable {
+  associatedtype Entity
+  func toEntity() throws -> Entity
+}
