0f60d2e82f50ae070e1385d5740cbeb99d221745
ir.__.si <cocoa9805@naver.com>
2025-03-26 09:42:08 +0900

    feat: #28 꽃 장소 조회 관련 모델링 연결

---
+++ Projects/Data/FlowerSpot/FlowerSpotDataInterface/Sources/FlowerSpotEndpoint.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerSpotEndpoint.swift
+//  FlowerSpotDataInterface
+//
+//  Created by 조용인 on 3/26/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import Foundation
+import Networker
+
+struct FlowerSpotEndpoint: Sendable {
+  
+  static func getFlowerSpotWithArea() -> Endpoint<EmptyRes> {
+    
+  }
+}
---
+++ Projects/Domain/FlowerSpot/FlowerSpotDomainInterface/Sources/Entity/FlowerSpotListEntity.swift
@@ -0,0 +1,13 @@
+//
+//  FlowerSpotListEntity.swift
+//  FlowerSpotDomainInterface
+//
+//  Created by 조용인 on 3/26/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import Foundation
+
+public struct FlowerSpotListEntity: Equatable, Sendable {
+  public init() { }
+}
