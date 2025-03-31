d81ea46f8df4e4d8796e0060b16f5021ceb846d2
ir.__.si <cocoa9805@naver.com>
2025-03-31 12:16:34 +0900

    feat: #39 TagView 추가

---
+++ Projects/Core/DesignKit/Sources/Component/TagView/TagView.swift
@@ -0,0 +1,27 @@
+//
+//  TagView.swift
+//  DesignKit
+//
+//  Created by 조용인 on 3/31/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import SwiftUI
+
+public struct TagView: View {
+  public let text: String
+  
+  public init(text: String) {
+    self.text = text
+  }
+  
+  public var body: some View {
+    Text(text)
+      .fontStyle(FontSet.Caption.caption1)
+      .foregroundColor(ColorSet.Text.Secondary)
+      .padding(.horizontal, .Number2)
+      .padding(.vertical, .Number6)
+      .background(ColorSet.Background.Tertiary)
+      .cornerRadius(.Number4)
+  }
+}
