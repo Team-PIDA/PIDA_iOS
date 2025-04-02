7f1bff6906cc759c86695fe289b8567f0903f2e1
ir.__.si <cocoa9805@naver.com>
2025-04-02 13:27:01 +0900

    chore: #50 FlowerSpotDetail 의존성 수정

--- Projects/Features/FlowerSpotDetail/Project.swift
+++ Projects/Features/FlowerSpotDetail/Project.swift
@@ -12,6 +12,7 @@ import ProjectDescriptionHelpers
 let project = Project.makeFeature(
   name: "FlowerSpotDetail",
   featureInterfaceDependencies: [
+    .InternalDependency.Core
     // 필요 시, Domain Interface Dependency 추가
   ]
 )
