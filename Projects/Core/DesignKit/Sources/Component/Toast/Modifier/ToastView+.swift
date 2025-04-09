b278938f7936558faaa252fb084ba15632dccc62
ir.__.si <yongin9805@gmail.com>
2025-04-09 13:34:44 +0900

    Refactor/update design system (#71)
    * refactor: #68 ToastView에 button 추가
    
    * refactor: #68 TagView icon 모디파이어 추가

---
+++ Projects/Core/DesignKit/Sources/Component/Toast/Modifier/ToastView+.swift
@@ -0,0 +1,22 @@
+//
+//  ToastView+.swift
+//  DesignKit
+//
+//  Created by 조용인 on 4/9/25.
+//  Copyright © 2025 com.yongin.pida. All rights reserved.
+//
+
+import SwiftUI
+
+public extension ToastView {
+  /// toast의 버튼이 탭되었을 때 실행될 동작을 설정합니다.
+  /// - Parameter action: 버튼이 탭되었을 때 실행될 클로저입니다.
+  /// - Returns: 지정된 동작이 적용된 수정된 버튼을 반환합니다.
+  func action(
+    _ action: @escaping () async -> Void
+  ) -> Self {
+    var toast = self
+    toast.action = action
+    return toast
+  }
+}
