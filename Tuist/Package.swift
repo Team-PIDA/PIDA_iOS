e85c32998150a1f67a3a438c6aa86c11eeae9a96
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2025-12-19 00:14:09 +0900

    fix: #82 swift-navigation 버전 설정

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -30,7 +30,8 @@ let package = Package(
   name: "PIDA_iOS",
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", exact: "1.18.0"),
-    .package(url: "https://github.com/navermaps/SPM-NMapsMap.git", exact: "3.20.0"),
+    .package(url: "https://github.com/pointfreeco/swift-navigation.git", exact: "2.3.1"), /// swift-navigation 명시적 버전 지정
+    .package(url: "https://github.com/navermaps/SPM-NMapsMap.git", exact: "3.21.0"),
     .package(url: "https://github.com/airbnb/lottie-ios.git", exact: "4.5.0"),
     .package(url: "https://github.com/LottieFiles/dotlottie-ios", exact: "0.8.0")
   ]
