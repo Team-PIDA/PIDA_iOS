567221626f2ced3c144f8856137f545f3a75e2b4
ir.__.si <cocoa9805@naver.com>
2025-12-23 16:21:51 +0900

    refactor: #81 Data/Domain 템플릿 삭제 및 Client 템플릿 추가

---
+++ Tuist/Templates/Client/Client.swift
@@ -0,0 +1,53 @@
+//
+//  Client.swift
+//  APIManifests
+//
+//  Created by 조용인 on 12/23/25.
+//
+
+import Foundation
+import ProjectDescription
+
+fileprivate let name: Template.Attribute = .required("name")
+fileprivate let author: Template.Attribute = .required("author")
+
+/// 파일 경로
+fileprivate let ClientPath: String = "Projects/Client/\(name)"
+
+let dataTemplate = Template(
+  description: "Client 모듈 템플릿",
+  attributes: [
+    name,
+    author
+  ],
+  items: [
+    .file(
+      path: "\(ClientPath)/\(name)/Project.swift",
+      templatePath: "client_project.stencil"
+    ),
+    .file(
+      path: "\(ClientPath)/\(name)/Sources/Client/\(name)Client.swift",
+      templatePath: "client_base.stencil"
+    ),
+    .file(
+      path: "\(ClientPath)/\(name)/Sources/Client/\(name)Client+Endpoint.swift",
+      templatePath: "client_endpoint.stencil"
+    ),
+    .file(
+      path: "\(ClientPath)/\(name)/Sources/Client/\(name)Client+Live.swift",
+      templatePath: "client_live.stencil"
+    ),
+    .file(
+      path: "\(ClientPath)/\(name)/Sources/DTO/Request/dummy.swift",
+      templatePath: "dummy.stencil"
+    ),
+    .file(
+      path: "\(ClientPath)/\(name)/Sources/DTO/Response/dummy.swift",
+      templatePath: "dummy.stencil"
+    ),
+    .file(
+      path: "\(ClientPath)/\(name)/Sources/Entity/dummy.swift",
+      templatePath: "dummy.stencil"
+    ),
+  ]
+)
