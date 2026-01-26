4ae031486b4de3e1c129f0c9eab63f707bde9dd7
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2026-01-26 13:53:32 +0900

    fix: SearchRegionList Dependency 설정 누락 수정

--- Projects/Feature/SearchRegionList/Project.swift
+++ Projects/Feature/SearchRegionList/Project.swift
@@ -12,7 +12,8 @@ import ProjectDescriptionHelpers
 let project = Project.buildFeature(
   for: Feature.SearchRegionList,
   interfaceDependencies: [
-    .Client.Search
+    .Client.Search,
+    .Client.FlowerSpot
     // 필요한 Domain Interface 의존성 추가
   ]
 )
