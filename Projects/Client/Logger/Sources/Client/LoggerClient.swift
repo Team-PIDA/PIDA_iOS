dce32ca4d073d693e24503aff2a70014f28d7c56
ji-yeon224 <69784492+ji-yeon224@users.noreply.github.com>
2026-02-11 00:52:46 +0900

    refactor: LogLevel enum을 별도 파일로 분리 및 코드 정리
    - LogLevel enum을 LoggerClient.swift에서 독립적인 LogLevel.swift 파일로 이동
    - LoggerClient.swift에서 불필요한 주석 제거
    - 모듈 구조 개선으로 관심사 분리 향상

--- Projects/Client/Logger/Sources/Client/LoggerClient.swift
+++ Projects/Client/Logger/Sources/Client/LoggerClient.swift
@@ -9,14 +9,6 @@
 import ComposableArchitecture
 import OSLog
 
-public enum LogLevel: Sendable {
-  case debug
-  case info
-  case notice
-  case error
-  case fault
-}
-
 @DependencyClient
 public struct LoggerClient: Sendable {
   public var log: @Sendable (_ message: String, _ level: LogLevel) -> Void
@@ -24,7 +16,7 @@ public struct LoggerClient: Sendable {
 }
 
 public extension DependencyValues {
-  var loggerClient /*<- 카멜케이스 적용*/ : LoggerClient {
+  var loggerClient: LoggerClient {
     get { self[LoggerClient.self] }
     set { self[LoggerClient.self] = newValue }
   }
---
+++ Projects/Client/Logger/Sources/Model/LogLevel.swift
@@ -0,0 +1,17 @@
+//
+//  LogLevel.swift
+//  LoggerClient
+//
+//  Created by Jiyeon on 2/11/26.
+//  Copyright © 2026 com.pida.me. All rights reserved.
+//
+
+import Foundation
+
+public enum LogLevel: Sendable {
+  case debug
+  case info
+  case notice
+  case error
+  case fault
+}
